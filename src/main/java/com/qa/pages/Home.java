94b45614aa59fd4150789b014d252f3d2a1225f3
Ravindra P <ravindra@ICREANET.NET>
2019-08-07 15:35:14 +0530

    My first commit for AppForCare

---
+++ src/main/java/com/qa/pages/Home.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class Home {
+	
+	public void homepage()
+	{
+		System.out.println("this is home page");
+	}
+
+}
---
+++ src/main/java/com/qa/pages/Login.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class Login {
+
+	public void login()
+	{
+		System.out.println("Login method");
+	}
+}
